1b694f873019a5e3a6664974cdadf1a127b9fb9f
Francisco Arevalo <arevalofrancisco77@gmail.com>
2020-05-21 00:35:31 -0700

    proof of concept
    The code works as I left it last time. This is a proof of concept that just needs to be implemented

--- MasterDevice2ndAttempt/MasterDevice2ndAttempt.ino
+++ MasterDevice2ndAttempt/MasterDevice2ndAttempt.ino
@@ -1,10 +1,26 @@
 //Master Device YouTube Glove
-void setup() {
-  // put your setup code here, to run once:
+//This is the device that is connected to the glove itself
+//This will be the Micro
 
+int FirstFinger = 9;
+
+void setup()
+{ 
+  pinMode(FirstFinger, INPUT_PULLUP);
+  digitalWrite(FirstFinger, HIGH);
+  Serial1.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+  if (digitalRead(FirstFinger) == LOW)
+  {
+    Serial1.write('1');
+    delay(150);
+  }
+  else
+  {
+    Serial1.write('N');
+    delay(150);
+  }
 }
--- SlaveDevice2ndAttempt/SlaveDevice2ndAttempt.ino
+++ SlaveDevice2ndAttempt/SlaveDevice2ndAttempt.ino
@@ -1,10 +1,35 @@
 //Slave device YouTube Glove
-void setup() {
-  // put your setup code here, to run once:
+//This is the device that will be connected to the computer
+//This is the Arduino Uno 
 
+//#include <Keyboard.h>
+
+//An LED that is used to indicate whether we recieved something from the serial monitor
+int greenLEDPin = 9;
+int state = 0;
+void setup()
+{
+  pinMode(greenLEDPin, INPUT);
+  digitalWrite(greenLEDPin, LOW);
+  Serial.begin(9600);
+ // Keyboard.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  //Read what is in the serial port and save it to state
+  state = Serial.read();
 
+  if (state == '1')
+  {
+    Serial.print("I got your message. LED ON!\n");
+    digitalWrite(greenLEDPin, HIGH);
+    delay(150);
+  }
+  else
+  {
+    Serial.print("No message recieved. LED OFF!\n");
+    digitalWrite(greenLEDPin, LOW);
+    delay(150);
+  }
 }
